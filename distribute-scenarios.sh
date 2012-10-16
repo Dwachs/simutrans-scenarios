9a25a3a7791bc619e24b7658ddd1907ddd47d4da
Dwachs <dwachs@gmx.net>
2012-10-16 21:06:34 +0200

    add script to produce distributable archive files

---
+++ distribute-scenarios.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+
+zip simutrans-scenarios-pak64-$1.zip -r simutrans/pak/scenario
+zip simutrans-scenarios-pak128-ny-$1.zip -r simutrans/pak128/scenario/new_york
+zip simutrans-scenarios-pak128-tramadness-$1.zip -r simutrans/pak128/scenario/tramadness
+zip simutrans-scenarios-pak128-sell_clothing-$1.zip -r simutrans/pak128/scenario/sell_clothing
